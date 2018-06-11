6247e7765a6bcf36b5502c55bc48940ee3fe44ac
Fabio Daniele <fabio@apical.me>
2018-06-11 18:01:21 +0200

    Added 'commander'; added test command; updated usage of 'fs' code

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 // Getting the main class
 var Mage2TS = require('./lib/tools.js').Mage2TS;
+var commander = require('commander');
 
-// Defining utility vars
-var validParams = {
-    host: new RegExp("--(host)=([a-zA-Z0-9\.://]+)"),
-    store: new RegExp("--(store)=([a-zA-Z0-9\.://]+)"),
-    class: new RegExp("--(class)=([a-zA-Z0-9\.://]+)"),
-    firebase: new RegExp("--(firebase)=(true|false)")
-};
-var _param, _key;
-
-// Parsing input and inflating Mage2TS object
-process.argv.forEach(function(val, index) {
-    if (index < 2)
-        return;
-
-    _key = val.match(new RegExp("--(.*)="))[1];
-
-    if (_param = val.match(validParams[_key]))
-        Mage2TS.data[_param[1]] = _param[2];
-    else
-        console.log("Parameter '" + _key + "' or its value was not recognized. Ignored.");
-});
-
-// Effectively launching get & parse
-Mage2TS.init();
+commander
+    .version('0.1.2', '-v, --version')
+    .description('Converts Magento\'s Swagger schema into a Typescript class. Firebase ready.')
+    //.command('mage2ts')
+    .arguments('<host>')
+    .option('-s, --store <string>', 'the store code from which to gain the Swagger scheme (default to "default")')
+    .option('-c, --class <string>', 'name of the generated Typescript class (default to "Magento")')
+    .option('-f, --firebase', 'if added, the generated class will be put under src/functions (default to "src")')
+    .option('-t, --test', 'if added, Swagger schema will be processed but no class is generated')
+    .action(function(host) {
+        Mage2TS.init(host, commander.store, commander.class, commander.firebase, commander.test);
+    })
+    .parse(process.argv);
